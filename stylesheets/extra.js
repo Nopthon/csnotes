000c8ae057e2651e9a168dc2d1e7f0390602a2f3
Nopthon <nopthon@qq.com>
2026-01-06 14:58:18 +0800

    Deployed 6271d31 with MkDocs version: 1.6.1

--- stylesheets/extra.js
+++ stylesheets/extra.js
@@ -1,3 +1,22 @@
+// external-links
+document.addEventListener("DOMContentLoaded", function() {
+  const externalLinks = document.querySelectorAll('a[href^="http://"], a[href^="https://"]');
+
+  const currentHost = window.location.hostname;
+  
+  externalLinks.forEach(link => {
+    try {
+      const url = new URL(link.href);
+      if (url.hostname !== currentHost) {
+        link.target = "_blank";
+        link.rel = "noopener noreferrer";
+      }
+    } catch (e) {
+      // neglect
+    }
+  });
+});
+
 // KaTeX
 
 document$.subscribe(({ body }) => {
